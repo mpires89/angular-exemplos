d4ca938261ef79f36a457cdccd0094cb1b56f272
mpires89 <mpires2310@gmail.com>
2018-10-28 22:32:58 -0300

    .

--- src/app/components/pessoa/inserir/pessoa.component.ts
+++ src/app/components/pessoa/inserir/pessoa.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Endereco } from '../../models/endereco';
 import { PessoaModule } from '../../models/pessoa.modulo';
 import { PessoaService } from '../../server/pessoa.service';
+import { ActivatedRoute, Router } from '../../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-pessoa',
@@ -9,20 +10,39 @@ import { PessoaService } from '../../server/pessoa.service';
   styleUrls: ['./pessoa.component.css']
 })
 export class PessoaComponent implements OnInit {
-  public endereco: Endereco
-  public pessoa: PessoaModule
-
-  constructor(private service: PessoaService) { }
+  public endereco: Endereco;
+  public pessoa: PessoaModule;
+  cpfURL: string;
+  
+  constructor(private service: PessoaService,private route: ActivatedRoute, 
+    private router: Router) { }
   
   ngOnInit() {
     this.pessoa = new PessoaModule();
+
+    this.cpfURL = this.route.snapshot.paramMap.get('cpf');
+
+    if(this.cpfURL){
+      this.buscarPorCPF();
+    }
   }
   
-  salvar(){
-    this.service.postBanco(this.pessoa).subscribe(p => this.pessoa = p);
-    this.pessoa = new PessoaModule();
+
+  cadastrar(): void{
+    if(this.pessoa.cpf){
+      this.service.alterarPessoa(this.pessoa)
+      .subscribe(() => {this.pessoa = new PessoaModule()
+      this.router.navigate(['/pessoa/pessoa-list']);
+      });
+    } else{
+      this.service.postBanco(this.pessoa).subscribe(() => {this.pessoa = new PessoaModule()
+      this.router.navigate(['/pessoa/pessoa-list']);
+      });
+    }
+  }
 
 
-
+  buscarPorCPF(): void{
+    this.service.getPessoaPorCpf(this.cpfURL).subscribe(pessoaRecebida => this.pessoa = pessoaRecebida);
   }
 }
--- src/app/components/server/pessoa.service.ts
+++ src/app/components/server/pessoa.service.ts
@@ -7,6 +7,8 @@ import { PessoaModule } from '../models/pessoa.modulo';
 const URL_BUSCAR_PESSOAS: string = 'http://localhost:8080/pessoa/buscarTodos';
 const URL_SALVAR_PESSOA: string = 'http://localhost:8080/pessoa/inserir';
 const URL_EXCLUIR_PESSOA: string = 'http://localhost:8080/pessoa/excluir/';
+const URL_API_ALTERAR = 'http://localhost:8080/pessoa/alterar';
+const URL_API_CPF = 'http://localhost:8080/pessoa/buscarPorCpf/';
 
 
 @Injectable({
@@ -28,8 +30,12 @@ export class PessoaService {
   excluirPessoa(cpf: string):Observable<void>{
     return this.http.delete<void>(URL_EXCLUIR_PESSOA+cpf)
   }
-  
-
+  alterarPessoa(pessoa: PessoaModule): Observable<void> {
+    return this.http.put<void>(URL_API_ALTERAR, pessoa);
+  }
+  getPessoaPorCpf(cpf:string): Observable<PessoaModule> {
+    return this.http.get<PessoaModule>(URL_API_CPF + cpf);
+  }
 
   
 
